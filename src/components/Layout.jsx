78f94501ae43fcda0c4fac03a7e4c53dd4f85558
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-17 10:56:54 -0300

    feat: Criação do component de Layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Layout() {
+  return <>
+    <h1>PetShop</h1>
+    <nav>
+        <Link href="/" > Blog </Link>
+        <Link href="/produtos" > Produtos </Link>
+        <Link href="/sobre" > Sobre </Link>
+        <Link href="/contato" > Contato </Link>
+    </nav>
+  </>;
+}
