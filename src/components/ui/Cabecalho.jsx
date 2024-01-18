acdee40e8c372ed4cc9331a75a9c5d384b414c34
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-18 08:45:38 -0300

    feat: atributo image no h1/ logo

--- src/components/ui/Cabecalho.jsx
+++ src/components/ui/Cabecalho.jsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
 import Menu from "./Menu";
+import Image from "next/image";
+
 
 export default function Cabecalho() {
   return (
     <header>
       <h1>
+        <Image src="/images/logo.png" width={48} height={48} alt="Pata dentro de um coração" />
         <Link href="/">PetShop</Link>
       </h1>
       <Menu />
