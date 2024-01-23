b6d2f08e280042738b31638c0044e70adcfbb76f
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-23 11:05:23 -0300

    feat: estrutura da página  [id].jsx

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,3 +1,24 @@
+import Container from "@/components/ui/Container";
+import Head from "next/head";
+import styled from "styled-components";
+
+const StyledPost = styled.article``;
+
 export default function Post() {
-  return <h2>Página Post</h2>;
+  return (
+    <>
+      <Head>
+        <title>Titulo do Post.... - PetShop 2024</title>
+        <meta name="description" content="Descrição do post..." />
+      </Head>
+      <StyledPost>
+        <h2>Título do Post</h2>
+
+        <Container>
+          <h3>Categoria...</h3>
+          <p>Descrição do post...</p>
+        </Container>
+      </StyledPost>
+    </>
+  );
 }
