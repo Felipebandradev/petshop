cdbb91c2cb99ea3ac54f1c4d54b702756cfc37a8
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-23 11:31:47 -0300

    feat: Configuração do getStaticProps

--- src/pages/posts/[id].jsx
+++ src/pages/posts/[id].jsx
@@ -1,6 +1,7 @@
 import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
+import serverApi from "../api/server";
 
 const StyledPost = styled.article`
   h2::before {
@@ -9,15 +10,31 @@ const StyledPost = styled.article`
 `;
 
 export async function getStaticProps({ params }) {
-    const {id} = params;
-    
+  const { id } = params;
+
+  try {
+    const resposta = await fetch(`${serverApi}/posts/${id}`);
+
+    if (!resposta.ok) {
+      throw new Error(`Erro: ${resposta.status} - ${resposta.statusText}`);
+    }
+    const dados = await resposta.json();
+
+    return {
+      props: {
+        post: dados,
+      },
+    };
+  } catch (error) {
+    console.error("Deu ruim: " + error.message);
+  }
 }
 
-export async function getStaticPaths(){
-    return{
-        paths:[],
-        fallback:"blocking"
-    }
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
 }
 
 export default function Post() {
