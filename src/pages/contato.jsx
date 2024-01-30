7d632461c9bee3ab87d66e98b89a220ae7a37508
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-01-30 11:55:37 -0300

    feat: enviar Contato

--- src/pages/contato.jsx
+++ src/pages/contato.jsx
@@ -2,6 +2,8 @@ import Container from "@/components/ui/Container";
 import Head from "next/head";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
+import serverApi from "./api/server";
+import { useRouter } from "next/router";
 
 const StyledContato = styled.section`
   h2::before {
@@ -40,9 +42,25 @@ const StyledContato = styled.section`
 
 export default function Contato() {
   const { register, handleSubmit } = useForm();
+  let router = useRouter();
 
-  const enviarContato = (dados) => {
-    console.log("Enviando dados...",dados);
+  const enviarContato = async (dados) => {
+    const { nome, email, mensagem } = dados;
+    const opcoes = {
+      method: "POST",
+      body: JSON.stringify({ nome, email, mensagem }),
+      header: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    };
+
+    try {
+      await fetch(`${serverApi}/contatos.json`, opcoes);
+      alert("Dados enviados!");
+      router.push("/");
+    } catch (error) {
+      console.error("Deu ruim no envi: " + error.message);
+    }
   };
 
   return (
@@ -88,9 +106,9 @@ export default function Contato() {
                 Menssagem: <br />
               </label>
               <textarea
-                {...register("menssagen")}
-                name="menssagen"
-                id="menssagem"
+                {...register("mensagem")}
+                name="mensagem"
+                id="mensagem"
                 maxLength={500}
                 cols="30"
                 rows="10"
